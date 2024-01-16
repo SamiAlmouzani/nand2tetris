7a80426c9751a93199d818b157a13242d62696ed
samialmouzani <salmouza@gmail.com>
2024-01-16 15:59:36 -0500

    add push constant and push/pop local

--- projects/07/MemoryAccess/code_writer.py
+++ projects/07/MemoryAccess/code_writer.py
@@ -1,6 +1,5 @@
 
 class CodeWriter:
-
     def __init__(self) -> None:
         self.file = open('BasicTest.asm', 'w', encoding='UTF-8')
     
@@ -9,8 +8,51 @@ class CodeWriter:
         pass 
 
     def write_push_pop(self, cmd: str, segment: str, index: int) -> None:
-        if cmd == 'C_PUSH' and segment == 'constant':
-            self.file.write(f"// D = {index}\n@{index}\nD=A\n// Ram[SP] = D\n@SP\nA=M\nM=D\n// SP++\n@SP\nM=M+1\n\n")
 
+        if cmd == 'C_PUSH' and segment == 'constant':
+            self.file.write(f"""//push-constant-i
+                            // D = {index}
+                            @{index}
+                            D=A
+                            // Ram[SP] = D
+                            @SP
+                            A=M
+                            M=D
+                            // SP++
+                            @SP
+                            M=M+1\n\n""".replace(" ", ""))
+        elif cmd == 'C_POP' and segment == 'local':
+            self.file.write(f"""// pop-local-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @LCL
+                            D = D + M
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+        elif cmd == 'C_PUSH' and segment == 'local':
+            self.file.write(f"""// push-local-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @LCL
+                            D = D + M
+                            A = D
+                            D = M
+                            // Ram[SP] = Ram[addr]
+                            @SP
+                            A = M
+                            M = D
+                            // SP++
+                            @SP
+                            M = M + 1\n\n""".replace(" ", ""))
     def close(self) -> None:
         self.file.close()
