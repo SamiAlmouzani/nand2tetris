5fe06a2466eb635a949355fd107027c034b4bc38
samialmouzani <salmouza@gmail.com>
2024-01-16 21:41:53 -0500

    vm emulator works for BasicTest

--- projects/07/MemoryAccess/code_writer.py
+++ projects/07/MemoryAccess/code_writer.py
@@ -1,15 +1,57 @@
 
 class CodeWriter:
     def __init__(self) -> None:
-        self.file = open('BasicTest.asm', 'w', encoding='UTF-8')
+        self.file = open('BasicTest/BasicTest.asm', 'w', encoding='UTF-8')
     
 
     def write_arithmetic(self, cmd: str) -> None:
+        if cmd == 'add':
+            self.file.write(f"""//add
+                            @SP
+                            M = M - 1 // SP--
+                            @SP
+                            A = M
+                            D = M // D = Ram[SP]  
+                            @SP
+                            M = M - 1 // SP--
+                            @R13
+                            M = D // Ram[13] = D
+                            @SP
+                            A = M
+                            D = M // D = Ram[SP]
+                            @R13
+                            D = D + M // D = D + Ram[13]
+                            @SP
+                            A = M
+                            M = D // Ram[SP] = D
+                            @SP
+                            M = M + 1 // SP++\n\n""".replace(" ", ""))
+        if cmd == 'sub':
+            self.file.write(f"""//sub
+                            @SP
+                            M = M - 1               
+                            @SP
+                            A = M
+                            D = M // D = Ram[SP]  
+                            @SP
+                            M = M - 1 // SP--
+                            @R13
+                            M = D // Ram[13] = D
+                            @SP
+                            A = M
+                            D = M // D = Ram[SP]
+                            @R13
+                            D = D - M // D = D - Ram[13]
+                            @SP
+                            A = M
+                            M = D // Ram[SP] = D
+                            @SP
+                            M = M + 1 // SP++\n\n""".replace(" ", ""))
         pass 
 
     def write_push_pop(self, cmd: str, segment: str, index: int) -> None:
-
-        if cmd == 'C_PUSH' and segment == 'constant':
+        
+        if cmd == 'push' and segment == 'constant':
             self.file.write(f"""//push-constant-i
                             // D = {index}
                             @{index}
@@ -21,29 +63,13 @@ class CodeWriter:
                             // SP++
                             @SP
                             M=M+1\n\n""".replace(" ", ""))
-        elif cmd == 'C_POP' and segment == 'local':
-            self.file.write(f"""// pop-local-i
-                            // addr <- lcl + i
-                            @{index}
-                            D = A
-                            @LCL
-                            D = D + M
-                            @R13 // holds addr
-                            M = D
-                            // SP--
-                            @SP
-                            M = M - 1
-                            D = M
-                            // Ram[addr] = Ram[SP]
-                            @R13
-                            A = M
-                            M = D\n\n""".replace(" ", ""))
-        elif cmd == 'C_PUSH' and segment == 'local':
+        elif cmd == 'push' and segment == 'local':
+            seg = 'LCL'
             self.file.write(f"""// push-local-i
                             // addr <- lcl + i
                             @{index}
                             D = A
-                            @LCL
+                            @{seg}
                             D = D + M
                             A = D
                             D = M
@@ -54,5 +80,211 @@ class CodeWriter:
                             // SP++
                             @SP
                             M = M + 1\n\n""".replace(" ", ""))
+    
+        elif cmd == 'pop' and segment == 'local':
+            seg = 'LCL'
+            self.file.write(f"""// pop-local-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            A = M 
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+        elif cmd == 'push' and segment == 'argument':
+            seg = 'ARG'
+            self.file.write(f"""// push-argument-i
+                            // addr <- arg + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            A = D
+                            D = M
+                            // Ram[SP] = Ram[addr]
+                            @SP
+                            A = M
+                            M = D
+                            // SP++
+                            @SP
+                            M = M + 1\n\n""".replace(" ", ""))
+    
+        elif cmd == 'pop' and segment == 'argument':
+            seg = 'ARG'
+            self.file.write(f"""// pop-argument-i
+                            // addr <- arg + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            A = M 
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+        elif cmd == 'push' and segment == 'this':
+            seg = 'THIS'
+            self.file.write(f"""// push-this-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            A = D
+                            D = M
+                            // Ram[SP] = Ram[addr]
+                            @SP
+                            A = M
+                            M = D
+                            // SP++
+                            @SP
+                            M = M + 1\n\n""".replace(" ", ""))
+    
+        elif cmd == 'pop' and segment == 'this':
+            seg = 'THIS'
+            self.file.write(f"""// pop-this-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            A = M
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+ 
+        elif cmd == 'push' and segment == 'that':
+            seg = 'THAT'
+            self.file.write(f"""// push-that-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            A = D
+                            D = M
+                            // Ram[SP] = Ram[addr]
+                            @SP
+                            A = M
+                            M = D
+                            // SP++
+                            @SP
+                            M = M + 1\n\n""".replace(" ", ""))
+    
+        elif cmd == 'pop' and segment == 'that':
+            seg = 'THAT'
+            self.file.write(f"""// pop-that-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @{seg}
+                            D = D + M
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            A = M
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+        elif cmd == 'push' and segment == 'static':
+            self.file.write(f"""// push-static-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @Foo.{index}
+                            D = D + M
+                            A = D
+                            D = M
+                            // Ram[SP] = Ram[addr]
+                            @SP
+                            A = M
+                            M = D
+                            // SP++
+                            @SP
+                            M = M + 1\n\n""".replace(" ", ""))
+    
+        elif cmd == 'pop' and segment == 'static':
+            self.file.write(f"""// pop-static-i
+                            // addr <- lcl + i
+                            @{index}
+                            D = A
+                            @Foo.{seg}
+                            D = D + M
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            A = M
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+
+        elif cmd == 'push' and segment == 'temp':
+            self.file.write(f"""// push-temp-i
+                            // addr <- lcl + i
+                            @5
+                            D = A
+                            @{index}
+                            A = D + A
+                            D = M
+                            // Ram[SP] = Ram[addr]
+                            @SP
+                            A = M
+                            M = D
+                            // SP++
+                            @SP
+                            M = M + 1\n\n""".replace(" ", ""))
+    
+        elif cmd == 'pop' and segment == 'temp':
+            self.file.write(f"""// pop-temp-i
+                            // addr <- lcl + i
+                            @5
+                            D = A
+                            @{index}
+                            D = D + A
+                            @R13 // holds addr
+                            M = D
+                            // SP--
+                            @SP
+                            M = M - 1
+                            A = M
+                            D = M
+                            // Ram[addr] = Ram[SP]
+                            @R13
+                            A = M
+                            M = D\n\n""".replace(" ", ""))
+   
     def close(self) -> None:
+        self.file.write(f"""(END)
+                            @END
+                            0;JMP""".replace(" ", ""))
         self.file.close()
--- projects/07/MemoryAccess/parse.py
+++ projects/07/MemoryAccess/parse.py
@@ -15,28 +15,14 @@ class CParser:
                 continue
 
             line = line.strip().split(' ')
-            cmd = self.command_type(line[0])
-
-            if cmd == 'C_ARITHMETIC':
-                # code_writer.write_arithmetic(cmd)
-                print(line[0])
-            else:
+            cmd = line[0]
+            if cmd == 'push' or cmd == 'pop':
                 arg1: str = line[1]
                 arg2: str = line[2]
-
                 code_writer.write_push_pop(cmd, arg1, arg2)
                 print(f"cmd: {cmd}, arg1: {arg1}, arg2: {arg2}.")
+            else:
+                code_writer.write_arithmetic(cmd)
+                print(line[0])
 
         code_writer.close()
-    def command_type(self, cmd: str) -> str:
-        if cmd == 'add' or cmd == 'sub':
-            return 'C_ARITHMETIC' 
-        elif cmd == 'push':
-            return 'C_PUSH'
-        elif cmd == 'pop':
-            return 'C_POP'
-        else:
-            # if this got his, something sent wrong
-            return "something went wrong"
-
-
